0e60430924d49ead1299b1adfc48396e524f5e6b
Merbin J Anselm <merbinjanselm@gmail.com>
2020-02-18 19:22:13 +0530

    Added CORS

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/websocket v1.2.0
 	github.com/jinzhu/gorm v1.9.12
 	github.com/matoous/go-nanoid v1.1.0
+	github.com/rs/cors v1.7.0
 	github.com/vektah/gqlparser v1.2.0
 	github.com/volatiletech/authboss v2.4.0+incompatible
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
--- server/main.go
+++ server/main.go
@@ -12,46 +12,56 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/rs/cors"
 	"github.com/volatiletech/authboss"
 	"github.com/volatiletech/authboss/defaults"
 )
 
 var (
 	config *gkc.AppConfig
+	db     *gorm.DB
 )
 
 func main() {
 	config = gkc.DefaultAppConfig()
 
-	db := setupDB()
+	db = setupDB()
 	defer db.Close()
 
-	graphqlSchema := gkc.NewExecutableSchema(gkc.Config{
-		Resolvers: &gkc.Resolver{
-			DB: db,
-		},
-	})
+	handlerAuth := func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, gkc.CtxUserIDKey, "1") // TODO: Temperory solution for user info
+			h.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 
-	router := mux.NewRouter()
-	router.PathPrefix("/query").Handler(middlewareAuth(http.HandlerFunc(handler.GraphQL(graphqlSchema,
+	handlerCors := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			config.AppHost.String(),
+		},
+		AllowCredentials: true,
+	}).Handler
+
+	handlerGraphQL := handler.GraphQL(
+		gkc.NewExecutableSchema(gkc.Config{
+			Resolvers: &gkc.Resolver{
+				DB: db,
+			},
+		}),
 		handler.WebsocketUpgrader(websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
-				return true
-				// return r.URL.Hostname() == config.AppHost.Hostname() // TODO: Compute
+				return r.Host == config.AppHost.Host
 			},
 		}),
 		handler.WebsocketKeepAliveDuration(10*time.Second), // Don't drop websocket after being idle for few seconds https://github.com/99designs/gqlgen/issues/640
-	))))
+	)
+
+	router := mux.NewRouter()
+	router.Use(handlerCors, handlerAuth)
+	router.PathPrefix("/query").Handler(handlerGraphQL)
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir(config.StaticDir)))
 	log.Fatalf("Error running server -> %s", http.ListenAndServe(":"+config.AppHost.Port(), router))
-}
-
-func middlewareAuth(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		ctx = context.WithValue(ctx, gkc.CtxUserIDKey, "1") // TODO: Temperory solution for user info
-		h.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
 
 func setupDB() *gorm.DB {
