fe637f53dca462322574640b67bd8b82fce0acd0
Merbin J Anselm <merbinjanselm@gmail.com>
2020-02-27 20:06:01 +0530

    Updated login screen with logo & other text

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,17 +1,57 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
-import { Paper, TextField, Box, Button } from "@material-ui/core";
+import { Paper, TextField, Box, Button, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "@reach/router";
 
 const useStyles = makeStyles(theme => ({
+    pageWrapper: {
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column"
+    },
     pageContainer: {
         display: "flex",
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        height: "100vh"
+        flexGrow: "1"
     },
-    loginButton: {
+    boxWrapper: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    textWelcome: {
+        ...theme.custom.fontFamily.metropolis
+    },
+    textRegister: {
+        textDecoration: "none",
+        color: theme.palette.secondary.dark
+    },
+    textRegisterText: {
+        ...theme.custom.fontFamily.metropolis,
+        paddingTop: theme.spacing(3)
+    },
+    textAttribution: {
+        padding: theme.spacing(0, 2, 2, 0),
+        textAlign: "right"
+    },
+    textCreator: {
+        textDecoration: "none",
+        color: theme.palette.secondary.dark
+    },
+    loginButtonRoot: {
         marginTop: theme.spacing(3)
+    },
+    loginButtonText: {
+        ...theme.custom.fontFamily.metropolis,
+        color: theme.palette.secondary.contrastText,
+        textTransform: "capitalize"
+    },
+    logo: {
+        height: theme.spacing(7),
+        padding: theme.spacing(0, 0, 1, 0)
     },
     inputRoot: {
         '&$inputFocused $inputNotchedOutline': {
@@ -43,16 +83,20 @@ export default function () {
         }
     }
     return (
-        <>
+        <div className={classes.pageWrapper}>
             <Container maxWidth="xs" className={classes.pageContainer}>
                 <Paper elevation={3}>
-                    <Box p={3}>
+                    <Box className={classes.boxWrapper} p={3}>
+                        <img className={classes.logo} src={`../logo.png`} alt={"logo"} />
+                        <Typography className={classes.textWelcome} color="textSecondary" variant="subtitle1">Welcome back!</Typography>
                         <TextField InputLabelProps={inputLabelProps} InputProps={inputProps} label="Email" type="email" variant="outlined" fullWidth margin="normal" />
                         <TextField InputLabelProps={inputLabelProps} InputProps={inputProps} label="Password" type="password" variant="outlined" fullWidth margin="normal" />
-                        <Button className={classes.loginButton} variant="contained" color="secondary" disableElevation fullWidth size="large">Log In</Button>
+                        <Button classes={{ root: classes.loginButtonRoot, label: classes.loginButtonText }} variant="contained" color="secondary" disableElevation fullWidth size="large">Log In</Button>
                     </Box>
                 </Paper>
+                <Typography className={classes.textRegisterText} color="textSecondary" variant="body2">Don't have an account? <Link className={classes.textRegister} to="/register">Register</Link></Typography>
             </Container>
-        </>
+            <Typography className={classes.textAttribution} color="textSecondary" variant="body2">Created by <a className={classes.textCreator} href="https://github.com/anselm94">Merbin J Anselm</a></Typography>
+        </div>
     )
 }
